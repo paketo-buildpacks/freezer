6f6f7c2557984313fd73401e2a1e4e8dbfee098d
Jerico Pena <jericop@gmail.com>
2025-08-27 13:21:00 -0400

    Add functional options for offline and version

--- local_buildpack.go
+++ local_buildpack.go
@@ -15,7 +15,17 @@ func NewLocalBuildpack(path, name string) LocalBuildpack {
 	return LocalBuildpack{
 		Path:        path,
 		Name:        name,
-		UncachedKey: fmt.Sprintf("%s", name),
+		UncachedKey: name,
 		CachedKey:   fmt.Sprintf("%s:cached", name),
 	}
+}
+
+func (l LocalBuildpack) WithOffline(offline bool) LocalBuildpack {
+	l.Offline = offline
+	return l
+}
+
+func (l LocalBuildpack) WithVersion(version string) LocalBuildpack {
+	l.Version = version
+	return l
 }
